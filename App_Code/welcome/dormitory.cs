bd87eb5c3e1f9d87dd8155f8c44359e58f20605e
4615914@qq.com <4615914@qq.com>
2017-01-17 16:35:25 +0800

    宿舍20-22方法完成

--- App_Code/welcome/dormitory.cs
+++ App_Code/welcome/dormitory.cs
@@ -16,7 +16,7 @@ public class dormitory
 		//
 	}
     /// <summary>
-    /// 功能描述：学生是否已分配宿舍(学号):根据“学号”判断学生是否已经分配了宿舍，已分配返回true，否则返回false。
+    /// 功能描述：20学生是否已分配宿舍(学号):根据“学号”判断学生是否已经分配了宿舍，已分配返回true，否则返回false。
     /// 编写人：张明
     /// 创建时间：2017.1.17
     /// 更新记录：无
@@ -36,12 +36,13 @@ public class dormitory
         {
             try{new c_log().logAdd("dormitory.cs","isbillet", err.Message, "2", "zhangming1");//记录错误日志
             }catch{}
-            return false;
+            throw;
+            //return false;
         }
         return false;
     }
     /// <summary>
-    /// 功能描述：床位分配 学生已分配床位(学号):根据“学号”返回学生已分配的“床位分配”数据，否则返回null。
+    /// 功能描述：21床位分配 学生已分配床位(学号):根据“学号”返回学生已分配的“床位分配”数据，否则返回null。
     /// 编写人：张明
     /// 创建时间：2017.1.17
     /// 更新记录：无
@@ -62,9 +63,40 @@ public class dormitory
             try
             {
                 new c_log().logAdd("dormitory.cs", "billetdata", err.Message, "2", "zhangming1");//记录错误日志
+                throw;
             }
             catch { }
            
+        }
+        return billet;
+    }
+
+    /// <summary>
+    /// 功能描述：22床位 获取某床位数据(床位主键):根据“床位主键”返回“床位”数据table，否则返回空table。
+    /// 编写人：张明
+    /// 创建时间：2017.1.17
+    /// 更新记录：无
+    /// 版本记录：v0.0.1
+    /// </summary>
+    /// <param name="mkey">主键</param>
+    /// <returns>datatable</returns>
+    public static DataTable getbed(string mkey)
+    {
+        DataTable billet = new DataTable();
+        try
+        {
+            //到宿舍分配表中使用学号查询,并联查出相关床位分配信息；
+            billet = Sqlhelper.Serach("SELECT TOP 1 [PK_Bed_Log],[FK_Bed_NO],[FK_SNO],[Updater],[Update_DT]  FROM [Fresh_Bed_Log] where [FK_SNO]=@xh", new SqlParameter("xh", mkey));
+        }
+        catch (Exception err)
+        {
+            try
+            {
+                new c_log().logAdd("dormitory.cs", "billetdata", err.Message, "2", "zhangming1");//记录错误日志
+                throw;
+            }
+            catch { }
+
         }
         return billet;
     }
