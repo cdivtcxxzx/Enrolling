741b8391609aa8a49f42ba4b3497f7a04177374a
4615914@qq.com <4615914@qq.com>
2017-01-17 14:21:44 +0800

    宿舍管理类完成第一个方法

--- App_Code/welcome/dormitory.cs
+++ App_Code/welcome/dormitory.cs
@@ -16,11 +16,36 @@ public class dormitory
 		//
 	}
     /// <summary>
-    /// 学生是否已分配宿舍(学号):根据“学号”判断学生是否已经分配了宿舍，已分配返回true，否则返回false。
+    /// 功能描述：学生是否已分配宿舍(学号):根据“学号”判断学生是否已经分配了宿舍，已分配返回true，否则返回false。
+    /// 编写人：张明
+    /// 创建时间：2017.1.17
+    /// 更新记录：无
+    /// 版本记录：v0.0.1
     /// </summary>
     /// <param name="xh">学号</param>
     /// <returns>真假</returns>
     public static bool isbillet(string xh)
+    {
+        try
+        {
+            //到宿舍分配表中使用学号查询，如果记录大于0，即已分配；
+            DataTable billet = Sqlhelper.Serach("SELECT TOP 1 [FK_Bed_NO]  FROM [Fresh_Bed_Log] where [FK_SNO]=@xh", new SqlParameter("xh", xh));
+            if (billet.Rows.Count > 0) return true;
+        }
+        catch(Exception err)
+        {
+            try{new c_log().logAdd("dormitory.cs","isbillet", err.Message, "2", "system");//记录错误日志
+            }catch{}
+            return false;
+        }
+        return false;
+    }
+    /// <summary>
+    /// 学生是否已分配宿舍(学号):根据“学号”判断学生是否已经分配了宿舍，已分配返回true，否则返回false。
+    /// </summary>
+    /// <param name="xh">学号</param>
+    /// <returns>真假</returns>
+    public static bool isbillet2(string xh)
     {
         try
         {
@@ -34,4 +59,7 @@ public class dormitory
         }
         return false;
     }
+
+
+
 }
