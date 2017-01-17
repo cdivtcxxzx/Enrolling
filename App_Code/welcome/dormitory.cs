344e0885b84e47de5c47608fe48deeaf1a2c05a5
4615914 <4615914@qq.com>
2017-01-17 13:15:47 +0800

    宿舍管理类提交测试

--- App_Code/welcome/dormitory.cs
+++ App_Code/welcome/dormitory.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.Data;
+using System.Data.SqlClient;
 /// <summary>
 ///dormitory 宿舍服务类
 /// </summary>
@@ -14,4 +15,23 @@ public class dormitory
 		//TODO: 在此处添加构造函数逻辑
 		//
 	}
+    /// <summary>
+    /// 学生是否已分配宿舍(学号):根据“学号”判断学生是否已经分配了宿舍，已分配返回true，否则返回false。
+    /// </summary>
+    /// <param name="xh">学号</param>
+    /// <returns>真假</returns>
+    public static bool isbillet(string xh)
+    {
+        try
+        {
+            //到宿舍分配表中使用学号查询，如果记录大于0，即已分配；
+            DataTable billet = Sqlhelper.Serach("SELECT TOP 1 [FK_Bed_NO]  FROM [Fresh_Bed_Log] where [FK_SNO]=@xh", new SqlParameter("xh", xh));
+            if (billet.Rows.Count > 0) return true;
+        }
+        catch
+        {
+            return false;
+        }
+        return false;
+    }
 }
