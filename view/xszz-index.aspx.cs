828002d3f456eb7b73bf6f1c698527647c1a49d4
huyuanpzh <huyuanpzh@qq.com>
2017-03-28 16:17:35 +0800

    提交

--- view/xszz-index.aspx.cs
+++ view/xszz-index.aspx.cs
@@ -9,8 +9,10 @@ public partial class view_xszz_index : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        #region 控制业务代码
+        //访问本网页用户的授权验证
 
+
+        #region 控制业务代码
         this.pk_sno.Value = "2";//其值应由会话中来，在学生登陆成功后被赋予初值
 
 
