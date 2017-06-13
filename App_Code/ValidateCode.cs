be22d4a492fc60d07cc84ea8f961627bf5b3f926
webxwing <webxwing@163.com>
2017-06-13 09:04:37 +0800

    qiu-0613-验证码修改：全小写

--- App_Code/ValidateCode.cs
+++ App_Code/ValidateCode.cs
@@ -18,7 +18,7 @@ using System.Drawing.Imaging;
 /// </summary>
 public class validatedCode
 {
-    #region  验证码长度(默认6个验证码的长度)
+    #region  验证码长度(默认4个验证码的长度)
     int length = 4;
     public int Length
     {
@@ -91,7 +91,7 @@ public class validatedCode
     #endregion
 
     #region 自定义随机码字符串序列(使用逗号分隔)
-    string codeSerial = "1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+    string codeSerial = "1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,p,q,r,s,t,u,v,w,x,y,z";
     public string CodeSerial
     {
         get { return codeSerial; }
