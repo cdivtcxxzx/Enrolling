5c37d8e0eb9aa0e2658e82abe36aab25204b3114
uinx1983@163.com <uinx1983@163.com>
2017-06-19 11:18:51 +0800

    解决重设班主任权限问题

--- App_Code/GZJW.cs
+++ App_Code/GZJW.cs
@@ -19,7 +19,7 @@ public class GZJW
     public bool SyncClass()
     {
         string year = DateTime.Today.Year.ToString();
-        DataTable dt = Sqlhelper.ConSerach(Sqlhelper.conStrzsgl, "select cl.bh PK_Class_NO,cl.xqdm FK_Campus_NO,spe.pk_spe FK_SPE_NO,zy.SPE_Code,cl.bjmc Name from gzjw.[PantoSchoolGJ].[dbo].[JX_Class] cl left join enrollment.yxxt_data.dbo.Base_SPE_ZYDM zy on cl.zydm=zy.zydm left join enrollment.yxxt_data.dbo.Fresh_SPE spe on zy.spe_code=spe.spe_code left join enrollment.yxxt_data.dbo.Fresh_Counseller coun on cl.bh=coun.fk_class_no  where cl.njdm=@njdm and spe.[year]=@njdm", new SqlParameter("njdm", year));
+        DataTable dt = Sqlhelper.ConSerach(Sqlhelper.conStrzsgl, "select cl.bh PK_Class_NO,cl.xqdm FK_Campus_NO,spe.pk_spe FK_SPE_NO,zy.SPE_Code,cl.bjmc Name,cl.njdm Year from gzjw.[PantoSchoolGJ].[dbo].[JX_Class] cl left join enrollment.yxxt_data.dbo.Base_SPE_ZYDM zy on cl.zydm=zy.zydm left join enrollment.yxxt_data.dbo.Fresh_SPE spe on zy.spe_code=spe.spe_code left join enrollment.yxxt_data.dbo.Fresh_Counseller coun on cl.bh=coun.fk_class_no  where cl.njdm=@njdm and spe.[year]=@njdm", new SqlParameter("njdm", year));
         DataSet tempDs = new DataSet();
         DataTable temp = dt.Clone();
         temp.TableName = "Fresh_Class";
@@ -33,10 +33,11 @@ public class GZJW
             x["FK_Campus_NO"] = dr["FK_Campus_NO"].ToString();
             x["FK_SPE_NO"] = dr["FK_SPE_NO"].ToString();
             x["Name"] = dr["Name"].ToString();
+            x["Year"] = dr["Year"].ToString();
             temp.Rows.Add(x);
         }
         tempDs.Tables.Add(temp);
-        Sqlhelper.GetUpdateDataSet("select PK_Class_NO,FK_Campus_NO,FK_SPE_NO,Name from Fresh_Class", tempDs, "Fresh_Class");
+        Sqlhelper.GetUpdateDataSet("select PK_Class_NO,FK_Campus_NO,FK_SPE_NO,Name,[Year] from Fresh_Class", tempDs, "Fresh_Class");
         //Sqlhelper.ExcuteNonQuery("delete FROM [dbo].[Fresh_Class] where PK_Class_NO in ( select PK_Class_NO from Fresh_Class left join Fresh_SPE on Fresh_Class.FK_SPE_NO=Fresh_SPE.PK_SPE where Fresh_SPE.[Year]=@njdm)", new SqlParameter("njdm", year));
         //DataTable Fresh_Class = new DataTable("Fresh_Class");
         //Fresh_Class.Columns.Add("PK_Class_NO", typeof(string));
@@ -81,12 +82,12 @@ public class GZJW
     public DataTable GetClassDT()
     {
         string year = DateTime.Today.Year.ToString();
-        return Sqlhelper.Serach("select cl.PK_Class_NO,spe.pk_spe,col.College_NO,col.Name College_Name,cl.name,yh.yhid,yh.xm,coun.Phone,coun.QQ from [Fresh_Class] cl left join Fresh_SPE spe on cl.FK_SPE_NO=spe.PK_SPE left join Base_College col on spe.FK_College_Code=col.PK_College left join Fresh_Counseller coun on cl.PK_Class_NO=coun.fk_class_no left join yonghqx yh on coun.fk_staff_no=yh.yhid where spe.[year]=@njdm", new SqlParameter("njdm", year));
+        return Sqlhelper.Serach("select cl.PK_Class_NO,spe.pk_spe,col.College_NO,col.Name College_Name,cl.name,yh.yhid,yh.xm,coun.Phone,coun.QQ from [Fresh_Class] cl left join Fresh_SPE spe on cl.FK_SPE_NO=spe.PK_SPE left join Base_College col on spe.FK_College_Code=col.PK_College left join Fresh_Counseller coun on cl.PK_Class_NO=coun.fk_class_no left join yonghqx yh on coun.fk_staff_no=yh.yhid where cl.[year]=@njdm", new SqlParameter("njdm", year));
     }
     public DataTable GetClassOutDT()
     {
         string year = DateTime.Today.Year.ToString();
-        return Sqlhelper.Serach("select cl.PK_Class_NO 班号,cl.name 班级名称,yh.yhid 辅导员帐号,yh.xm 辅导员姓名,coun.Phone 辅导员电话,coun.QQ 辅导员QQ号 from [Fresh_Class] cl left join Fresh_SPE spe on cl.FK_SPE_NO=spe.PK_SPE left join Base_College col on spe.FK_College_Code=col.PK_College left join Fresh_Counseller coun on cl.PK_Class_NO=coun.fk_class_no left join yonghqx yh on coun.fk_staff_no=yh.yhid where spe.[year]=@njdm", new SqlParameter("njdm", year));
+        return Sqlhelper.Serach("select cl.PK_Class_NO 班号,cl.name 班级名称,yh.yhid 辅导员帐号,yh.xm 辅导员姓名,coun.Phone 辅导员电话,coun.QQ 辅导员QQ号 from [Fresh_Class] cl left join Fresh_SPE spe on cl.FK_SPE_NO=spe.PK_SPE left join Base_College col on spe.FK_College_Code=col.PK_College left join Fresh_Counseller coun on cl.PK_Class_NO=coun.fk_class_no left join yonghqx yh on coun.fk_staff_no=yh.yhid where cl.[year]=@njdm", new SqlParameter("njdm", year));
     }
     public bool SetCounseller(string PK_Class_NO,string yhid,string phone,string qq)
     {
@@ -100,7 +101,7 @@ public class GZJW
         {
             if (Sqlhelper.ExcuteNonQuery("update Fresh_Counseller set FK_Staff_NO=@FK_Staff_NO,Phone=@Phone,QQ=@QQ where FK_Class_NO=@FK_Class_NO", new SqlParameter("FK_Class_NO", PK_Class_NO), new SqlParameter("FK_Staff_NO", yhid), new SqlParameter("Phone", phone), new SqlParameter("QQ", qq)) == 1)
             {
-                
+                new Power().AddLsz(yhid, "25");
                 return true; }
             return false;
         }
@@ -113,6 +114,7 @@ public class GZJW
                 return true; }
             return false;
         }
+
         return false;
     }
     public DataTable GetClass(string PK_Class_NO)
